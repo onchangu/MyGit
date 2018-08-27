84fc337430d40f559fab6664321ee460208eb12e
Robert Onchangu <ronchangu@gmail.com>
2018-08-27 06:58:32 -0700

    GuessNumber Test

---
+++ PracticeExamples/src/GuessNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+	
+	public static void main(String [] args)
+	{
+		int randomNumber, userInput, count = 0;
+		
+		do {
+			
+			// generate random number
+			randomNumber = (int)(20 * Math.random()+ 1 );
+			
+			// ask user input
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Please enter the number to between 1 and 20 \n");
+			userInput = scan.nextInt();
+			System.out.println("You guessed " + userInput + " and the computer generated " + randomNumber);
+			count++;
+			System.out.print("Tries = "+ count);
+			
+		} while(randomNumber !=userInput);
+		
+		
+		
+	}
+
+}
